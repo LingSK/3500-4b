dac49e6724d1ea5bccf7003e2d7fc80eda1040f8
Ling <slb54@rc09xcs213.managed.mst.edu>
2019-04-04 16:47:08 -0500

    r4

--- lings.tab.c
+++ lings.tab.c
@@ -1980,9 +1980,9 @@ yyreduce:
 #line 529 "lings.y" /* yacc.c:1646  */
     {
                     string lexeme = string((yyvsp[-4].text));
-					//if($2.type!=INT)
-						//yyerror("Arg 1 must be integer");
-                    if((yyvsp[-3].typeInfo).type != NULL_TYPE && (yyvsp[0].typeInfo).type == LIST)
+					if(scopeStack.top().findEntry(string((yyvsp[-4].text))).type!=INT)
+						yyerror("Arg 1 must be integer");
+                    if((yyvsp[-3].typeInfo).type != NULL_TYPE && (yyvsp[-3].typeInfo).type == LIST)
                         yyerror("Arg 2 cannot be list");
 						
 					/*
